3a790f01b9f17c8af095c4d8931dacd4ef66aa37
Brent Williams <brent.williams333@gmail.com>
2017-10-23 22:27:49 -0700

    Fixing static issue

--- src/main/java/com/cpe/musty/MustangHelperSpeechletRequestStreamHandler.java
+++ src/main/java/com/cpe/musty/MustangHelperSpeechletRequestStreamHandler.java
@@ -11,7 +11,11 @@ import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
  */
 public final class MustangHelperSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
 
-    private static final Set<String> supportedApplicationIds = new HashSet<String>();
+    private static final Set<String> supportedApplicationIds;
+    static {
+        supportedApplicationIds = new HashSet<String>();
+        supportedApplicationIds.add("amzn1.ask.skill.92223192-d2e2-4a84-89a7-a852805f9899");
+    }
 
     public MustangHelperSpeechletRequestStreamHandler() {
         super(new MustangHelperSpeechlet(), supportedApplicationIds);
