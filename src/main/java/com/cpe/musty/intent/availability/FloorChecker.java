dcac863656ce1a8d182dfe4d35b121b26476f829
Brent Williams <brent.williams333@gmail.com>
2017-10-25 23:41:21 -0700

    Formatting

--- src/main/java/com/cpe/musty/intent/availability/FloorChecker.java
+++ src/main/java/com/cpe/musty/intent/availability/FloorChecker.java
@@ -7,12 +7,23 @@ import org.json.JSONObject;
 
 import com.cpe.musty.utility.JsonReader;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 public class FloorChecker {
 
+    // @formatter:off
+    private static final Map<Integer, String> FLOOR_ENDPOINTS= ImmutableMap.of(
+            1, "http://lib.calpoly.edu/api/availability/1st",
+            2, "http://lib.calpoly.edu/api/availability/2nd",
+            3, "http://lib.calpoly.edu/api/availability/3rd",
+            4, "http://lib.calpoly.edu/api/availability/4th",
+            5, "http://lib.calpoly.edu/api/availability/5th");
+    // @formatter:on
+
+    private static final int AVAILABLE_STATUS = 0;
     private static final String STATUS_KEY = "status";
 
     public long findAvailableComputers(final String floorUtterance) {
@@ -20,14 +31,15 @@ public class FloorChecker {
         final JSONObject floorJson = getJsonFromEndpoint(endpoint);
         final List<JSONObject> computers = convertToComputerList(floorJson);
 
-        return computers.stream()
-                .filter(o -> o.getInt(STATUS_KEY) == 0).count();
+        return computers.stream().filter(o -> o.getInt(STATUS_KEY) == AVAILABLE_STATUS).count();
     }
 
     private String getEndpointForUtterance(final String utterance) {
-        // For now, just return the first floor endpoint.
-        // In the long run, we should be parsing the utterance and mapping it to a floor
-        // i.e. "one", "first", etc. all map to the below endpoint
+        /*
+         * For now, just return the first floor endpoint. In the long run, we
+         * should be parsing the utterance and mapping it to a floor, i.e.
+         * "one", "first", etc. all map to the below endpoint
+         */
         return "http://lib.calpoly.edu/api/availability/1st";
     }
 
@@ -40,9 +52,7 @@ public class FloorChecker {
     }
 
     private List<JSONObject> convertToComputerList(final JSONObject floorMap) {
-        return floorMap.toMap().values().stream()
-                .map(o -> new JSONObject((Map) o))
-                .filter(o -> o.has(STATUS_KEY))
+        return floorMap.toMap().values().stream().map(o -> new JSONObject((Map) o)).filter(o -> o.has(STATUS_KEY))
                 .collect(ImmutableList.toImmutableList());
     }
 }
